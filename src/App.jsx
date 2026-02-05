f83a1d0c7fa289ca6de837b170d356fbee321050
Saish Walavalkar <saishwalavalkar0205@gmail.com>
2026-02-05 23:16:13 +0530

    Frontend skeleton with components and pages

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind is finally working 🚀
+      </h1>
+    </div>
+  )
+}
+
+
